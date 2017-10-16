c3e41bb431d792f573461d6c2ce79ebe8c6cf817
Riley Shaw <rileyjshaw@gmail.com>
2017-10-16 15:42:41 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Simulation from './Flowers';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Simulation />, document.getElementById('root'));
-registerServiceWorker();
